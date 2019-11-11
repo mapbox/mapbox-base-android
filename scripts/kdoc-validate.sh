ae7e0d626ad7220179d406012d943f7653b79d4b
Łukasz Paczos <lukasz.paczos@mapbox.com>
2019-11-11 14:12:05 +0100

    dropped annotations-processor's documentation requirement

--- scripts/kdoc-validate.sh
+++ scripts/kdoc-validate.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 ./gradlew clean
 ./gradlew annotations:dokka -i | grep -i 'No documentation for com' && { echo 'kdoc validation failed'; exit 1; }
-./gradlew annotations-processor:dokka -i | grep -i 'No documentation for com' && { echo 'kdoc validation failed'; exit 1; }
 ./gradlew base:dokka -i | grep -i 'No documentation for com' && { echo 'kdoc validation failed'; exit 1; }
 exit 0
